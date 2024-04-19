36a5c150cf3ef6f393fe0de6671a4844f7cafe8b
Filiberto <filiberto.dev@gmail.com>
2024-04-19 05:15:27 +0200

    feat: todo comments plugin

--- lua/user/git-stuff.lua
+++ lua/user/git-stuff.lua
@@ -1,1 +1,2 @@
 lvim.builtin.gitsigns.current_line_blame = true
+
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -11,5 +11,14 @@ lvim.plugins = {
     --         require("auto-save").setup()
     --     end,
     -- },
-    { "christoomey/vim-tmux-navigator" }
+    { "christoomey/vim-tmux-navigator" },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
 }
