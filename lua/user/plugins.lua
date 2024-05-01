7ccc0806e0acd1d9e3de7f7806a87d8da5d41b3b
Filiberto <filiberto.dev@gmail.com>
2024-05-01 14:36:16 +0200

    feat: minimap

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -21,4 +21,14 @@ lvim.plugins = {
             -- refer to the configuration section below
         }
     },
+    {
+        'wfxr/minimap.vim',
+        build = "cargo install --locked code-minimap",
+        -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+        config = function()
+            vim.cmd("let g:minimap_width = 10")
+            vim.cmd("let g:minimap_auto_start = 1")
+            vim.cmd("let g:minimap_auto_start_win_enter = 1")
+        end,
+    },
 }
